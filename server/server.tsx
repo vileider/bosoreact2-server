e861f317ddd84e7a76e1cf2af9782eb247a58304
szymo <szymon.basiul@wp.pl>
2022-01-02 20:30:39 +0100

    Start by express configuration.

--- server/server.tsx
+++ server/server.tsx
@@ -1,1 +1,9 @@
-cokolwiek;
+// import express from "express";
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+	console.log("Server on!");
+});
