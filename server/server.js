91910548ecef9dcd8da881714b7d84c01bbb3914
vileider <andrewbagio@yahoo.com>
2022-03-22 22:46:55 +0000

    1st test

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-// import express from "express";
+import express from "express";
 
-const express = require("express")
-const mysql = require("mysql");
 const app = express();
 const port = process.env.PORT || 8000;
-const cors = require("cors");
-app.use(cors());
+import cors from "cors";
+ app.use(cors());
 app.use(express.json());
 
-app.use("/rpsplayer", require("./rpsPlayerRoutes.tsx"));
-app.use("/test111",(req,res)=>{
-	res.send({test:"123"})
-})
+import rpsPlayerRoutes from "./rpsPlayerRoutes.js";
+import memory from "./memory.js";
 
+app.use("/rpsplmayer", rpsPlayerRoutes);
+app.use("/memory", memory);
+app.use("/test",(req,res)=>{
+	res.json({test:"abc"})
+})
 app.listen(port, () => {
-	console.log("Server on!",port);
-});
-6
+	console.log("Server on!")
+})
